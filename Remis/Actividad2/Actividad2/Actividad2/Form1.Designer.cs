3f30fbd708e99a726ec2b276d49a8418eaa6c306
Matias <matiaslisandro9@gmail.com>
2023-10-03 20:06:44 -0300

    Trabajo Remises

---
+++ Remis/Actividad2/Actividad2/Actividad2/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace Actividad2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtCombustible = new System.Windows.Forms.TextBox();
+            this.txtSerie = new System.Windows.Forms.TextBox();
+            this.txtPatente = new System.Windows.Forms.TextBox();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.txtRemis = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Remis = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modelo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Patente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Serie = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Combustible = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Disponibilidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupCheckBox = new System.Windows.Forms.GroupBox();
+            this.lblDistancia = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.txtDistancia = new System.Windows.Forms.TextBox();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.btnCotizar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtCombustible);
+            this.groupBox1.Controls.Add(this.txtSerie);
+            this.groupBox1.Controls.Add(this.txtPatente);
+            this.groupBox1.Controls.Add(this.txtModelo);
+            this.groupBox1.Controls.Add(this.txtRemis);
+            this.groupBox1.Location = new System.Drawing.Point(183, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(252, 301);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Agenda";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 261);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 34);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Cargar Remis";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(87, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 34);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Eliminar Remis";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Combustible";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Serie";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Patente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Modelo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nro Remis";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(168, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Actualizar datos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCombustible
+            // 
+            this.txtCombustible.Location = new System.Drawing.Point(138, 199);
+            this.txtCombustible.Name = "txtCombustible";
+            this.txtCombustible.Size = new System.Drawing.Size(100, 20);
+            this.txtCombustible.TabIndex = 4;
+            // 
+            // txtSerie
+            // 
+            this.txtSerie.Location = new System.Drawing.Point(138, 156);
+            this.txtSerie.Name = "txtSerie";
+            this.txtSerie.Size = new System.Drawing.Size(100, 20);
+            this.txtSerie.TabIndex = 3;
+            // 
+            // txtPatente
+            // 
+            this.txtPatente.Location = new System.Drawing.Point(138, 114);
+            this.txtPatente.Name = "txtPatente";
+            this.txtPatente.Size = new System.Drawing.Size(100, 20);
+            this.txtPatente.TabIndex = 2;
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(138, 74);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(100, 20);
+            this.txtModelo.TabIndex = 1;
+            // 
+            // txtRemis
+            // 
+            this.txtRemis.Location = new System.Drawing.Point(138, 35);
+            this.txtRemis.Name = "txtRemis";
+            this.txtRemis.Size = new System.Drawing.Size(100, 20);
+            this.txtRemis.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Remis,
+            this.Modelo,
+            this.Patente,
+            this.Serie,
+            this.Combustible,
+            this.Disponibilidad});
+            this.dataGridView1.Location = new System.Drawing.Point(527, 74);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(643, 290);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.Tag = "Disponibilidad";
+            // 
+            // Remis
+            // 
+            this.Remis.HeaderText = "Remis";
+            this.Remis.Name = "Remis";
+            // 
+            // Modelo
+            // 
+            this.Modelo.HeaderText = "Modelo";
+            this.Modelo.Name = "Modelo";
+            // 
+            // Patente
+            // 
+            this.Patente.HeaderText = "Patente";
+            this.Patente.Name = "Patente";
+            // 
+            // Serie
+            // 
+            this.Serie.HeaderText = "Serie";
+            this.Serie.Name = "Serie";
+            // 
+            // Combustible
+            // 
+            this.Combustible.HeaderText = "Combustible";
+            this.Combustible.Name = "Combustible";
+            // 
+            // Disponibilidad
+            // 
+            this.Disponibilidad.HeaderText = "Disponibilidad";
+            this.Disponibilidad.Name = "Disponibilidad";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnCotizar);
+            this.groupBox2.Controls.Add(this.txtCategoria);
+            this.groupBox2.Controls.Add(this.txtDistancia);
+            this.groupBox2.Controls.Add(this.lblCategoria);
+            this.groupBox2.Controls.Add(this.lblDistancia);
+            this.groupBox2.Location = new System.Drawing.Point(183, 432);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(252, 213);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cotizacion";
+            // 
+            // groupCheckBox
+            // 
+            this.groupCheckBox.Location = new System.Drawing.Point(1176, 74);
+            this.groupCheckBox.Name = "groupCheckBox";
+            this.groupCheckBox.Size = new System.Drawing.Size(120, 290);
+            this.groupCheckBox.TabIndex = 4;
+            this.groupCheckBox.TabStop = false;
+            this.groupCheckBox.Text = "Marcar disponibilidad";
+            // 
+            // lblDistancia
+            // 
+            this.lblDistancia.AutoSize = true;
+            this.lblDistancia.Location = new System.Drawing.Point(44, 39);
+            this.lblDistancia.Name = "lblDistancia";
+            this.lblDistancia.Size = new System.Drawing.Size(51, 13);
+            this.lblDistancia.TabIndex = 0;
+            this.lblDistancia.Text = "Distancia";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(44, 96);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(52, 13);
+            this.lblCategoria.TabIndex = 1;
+            this.lblCategoria.Text = "Categoria";
+            // 
+            // txtDistancia
+            // 
+            this.txtDistancia.Location = new System.Drawing.Point(138, 36);
+            this.txtDistancia.Name = "txtDistancia";
+            this.txtDistancia.Size = new System.Drawing.Size(100, 20);
+            this.txtDistancia.TabIndex = 3;
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Location = new System.Drawing.Point(138, 93);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(100, 20);
+            this.txtCategoria.TabIndex = 4;
+            // 
+            // btnCotizar
+            // 
+            this.btnCotizar.Location = new System.Drawing.Point(87, 153);
+            this.btnCotizar.Name = "btnCotizar";
+            this.btnCotizar.Size = new System.Drawing.Size(75, 23);
+            this.btnCotizar.TabIndex = 5;
+            this.btnCotizar.Text = "Cotizar";
+            this.btnCotizar.UseVisualStyleBackColor = true;
+            this.btnCotizar.Click += new System.EventHandler(this.btnCotizar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1323, 655);
+            this.Controls.Add(this.groupCheckBox);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtCombustible;
+        private System.Windows.Forms.TextBox txtSerie;
+        private System.Windows.Forms.TextBox txtPatente;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.TextBox txtRemis;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Remis;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Modelo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Patente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Serie;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Combustible;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Disponibilidad;
+        private System.Windows.Forms.GroupBox groupCheckBox;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblDistancia;
+        private System.Windows.Forms.Button btnCotizar;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.TextBox txtDistancia;
+    }
+}
+
---
+++ Remis/Actividad2/Actividad2/Actividad2/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Actividad2
+{
+    public partial class Form1 : Form
+    {
+        internal class FilaDatos
+        {
+            public string Columna1 { get; set; }
+            public string Columna2 { get; set; }
+            public string Columna3 { get; set; }
+            public string Columna4 { get; set; }
+            public string Columna5 { get; set; }
+        }
+        private List<FilaDatos> listaDatos = new List<FilaDatos>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (AgregarNuevaFila())
+            {
+                LimpiarTextBox();
+            }
+        }
+
+        private void CheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            DataGridViewRow associatedRow = checkBox.Tag as DataGridViewRow;
+
+            bool checkBoxValue = checkBox.Checked;
+
+            if (associatedRow != null)
+            {
+                int columnaDisponibilidadIndex = dataGridView1.Columns["Disponibilidad"].Index;
+                associatedRow.Cells[columnaDisponibilidadIndex].Value = checkBoxValue ? "Disponible" : "No disponible";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EliminarFilaSeleccionada();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ActualizarFilaSeleccionada())
+            {
+                LimpiarTextBox();
+            }
+        }
+
+        private void btnCotizar_Click(object sender, EventArgs e)
+        {
+            Cotizar();
+        }
+
+        private void LimpiarTextBox()
+        {
+            txtRemis.Text = "";
+            txtModelo.Text = "";
+            txtPatente.Text = "";
+            txtSerie.Text = "";
+            txtCombustible.Text = "";
+            txtDistancia.Text = "";
+            txtCategoria.Text = "";
+        }
+
+        private bool AgregarNuevaFila()
+        {
+            string nroRemis = txtRemis.Text;
+            string modelo = txtModelo.Text;
+            string patente = txtPatente.Text;
+            string serie = txtSerie.Text;
+            string combustible = txtCombustible.Text;
+
+            if (CamposVacios(nroRemis, modelo, patente, serie, combustible))
+            {
+                MessageBox.Show("Debe completar todos los campos");
+                return false;
+            }
+
+            foreach (FilaDatos fila in listaDatos)
+            {
+                if (fila.Columna1 == nroRemis)
+                {
+                    MessageBox.Show("El número de remis ya existe");
+                    return false;
+                }
+                if (fila.Columna3 == patente)
+                {
+                    MessageBox.Show("La patente ya existe");
+                    return false;
+                }
+            }
+
+            FilaDatos nuevaFila = new FilaDatos
+            {
+                Columna1 = nroRemis,
+                Columna2 = modelo,
+                Columna3 = patente,
+                Columna4 = serie,
+                Columna5 = combustible
+            };
+
+            listaDatos.Add(nuevaFila);
+            AgregarFilaDataGridView(nuevaFila);
+            return true;
+        }
+
+        private void AgregarFilaDataGridView(FilaDatos fila)
+        {
+            dataGridView1.Rows.Add(fila.Columna1, fila.Columna2, fila.Columna3, fila.Columna4, fila.Columna5);
+
+            DataGridViewRow addedRow = dataGridView1.Rows[dataGridView1.Rows.Count - 2];
+
+            CheckBox newCheckBox = new CheckBox();
+            newCheckBox.Text = "Disponible";
+            newCheckBox.Checked = false;
+            newCheckBox.Tag = addedRow;
+            newCheckBox.CheckedChanged += new EventHandler(CheckBox_CheckedChanged);
+            newCheckBox.Location = new Point(10, groupCheckBox.Controls.Count * 25);
+            newCheckBox.Size = new Size(150, 20);
+            groupCheckBox.Controls.Add(newCheckBox);
+        }
+
+        private void EliminarFilaSeleccionada()
+        {
+            if (dataGridView1.CurrentRow.Cells[0].Value == null)
+            {
+                MessageBox.Show("No se puede eliminar una fila vacía");
+                return;
+            }
+
+            int rowIndex = dataGridView1.CurrentRow.Index;
+            listaDatos.RemoveAt(rowIndex);
+            dataGridView1.Rows.RemoveAt(rowIndex);
+        }
+
+        private bool ActualizarFilaSeleccionada()
+        {
+            string nroRemis = txtRemis.Text;
+            string modelo = txtModelo.Text;
+            string patente = txtPatente.Text;
+            string serie = txtSerie.Text;
+            string combustible = txtCombustible.Text;
+
+            if (CamposVacios(nroRemis, modelo, patente, serie, combustible))
+            {
+                MessageBox.Show("Debe completar todos los campos");
+                return false;
+            }
+
+            int rowIndex = dataGridView1.CurrentRow.Index;
+            listaDatos[rowIndex].Columna1 = nroRemis;
+            listaDatos[rowIndex].Columna2 = modelo;
+            listaDatos[rowIndex].Columna3 = patente;
+            listaDatos[rowIndex].Columna4 = serie;
+            listaDatos[rowIndex].Columna5 = combustible;
+
+            dataGridView1.CurrentRow.Cells[0].Value = nroRemis;
+            dataGridView1.CurrentRow.Cells[1].Value = modelo;
+            dataGridView1.CurrentRow.Cells[2].Value = patente;
+            dataGridView1.CurrentRow.Cells[3].Value = serie;
+            dataGridView1.CurrentRow.Cells[4].Value = combustible;
+
+            return true;
+        }
+
+        private bool CamposVacios(params string[] campos)
+        {
+            foreach (string campo in campos)
+            {
+                if (string.IsNullOrWhiteSpace(campo))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void Cotizar()
+        {
+            int distancia;
+            if (!int.TryParse(txtDistancia.Text, out distancia))
+            {
+                MessageBox.Show("Ingrese una distancia válida");
+                return;
+            }
+            string categoria = txtCategoria.Text;
+
+            // Realizar la lógica de cotización aquí
+            // ...
+        }
+    }
+
+}
